bf7145f2030a3b16009a4a52b594042ed9e60699
Baudouin Théobald <baudouin.theobald@student-cs.fr>
2019-04-03 10:09:01 +0300

    added context menu editor

---
+++ Qt source/context-menu-tuner/guientry.cpp
@@ -0,0 +1,6 @@
+#include "guientry.h"
+
+GuiEntry::GuiEntry()
+{
+
+}
---
+++ Qt source/context-menu-tuner/guientry.h
@@ -0,0 +1,12 @@
+#ifndef GUIENTRY_H
+#define GUIENTRY_H
+
+#include <QWidget>
+
+class GuiEntry
+{
+public:
+    GuiEntry();
+};
+
+#endif // GUIENTRY_H
